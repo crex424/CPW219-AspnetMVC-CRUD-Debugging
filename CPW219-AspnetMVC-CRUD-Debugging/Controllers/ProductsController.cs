6b59beab6bb98911cb77fa29d03f2f8b622db2d7
Campbell Rex <campbellrex@outlook.com>
2023-02-21 16:28:50 -0800

    Refactored index

--- CPW219-AspnetMVC-CRUD-Debugging/Controllers/ProductsController.cs
+++ CPW219-AspnetMVC-CRUD-Debugging/Controllers/ProductsController.cs
@@ -15,7 +15,9 @@ namespace CPW219_AspnetMVC_CRUD_Debugging.Controllers
 
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Product.ToListAsync());
+            List<Product> products = await _context.Product.ToListAsync();
+
+            return View(products);
         }
 
         public IActionResult Create()
